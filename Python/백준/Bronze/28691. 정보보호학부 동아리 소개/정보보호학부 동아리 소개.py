092b984dee6c2ccc5b1769b36ba7aa34699369b0
박재민 <96679652+Cartridge9@users.noreply.github.com>
2024-05-22 19:22:45 +0900

    [Bronze V] Title: 정보보호학부 동아리 소개, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ Python/백준/Bronze/28691. 정보보호학부 동아리 소개/정보보호학부 동아리 소개.py
@@ -0,0 +1,11 @@
+a = input()
+if a == "W":
+    print("WiCys")
+if a == "M":
+    print("MatKor")
+if a == "C":
+    print("CyKor")
+if a == "A":
+    print("AlKor")
+if a == "$":
+    print("$clear")

6d15e5ea31096b027b223ee6b78f679db286d61b
박재민 <96679652+Cartridge9@users.noreply.github.com>
2024-05-31 11:21:21 +0900

    [Bronze V] Title: 진흥원 세미나, Time: 44 ms, Memory: 31252 KB -BaekjoonHub

---
+++ Python/백준/Bronze/30087. 진흥원 세미나/진흥원 세미나.py
@@ -0,0 +1,5 @@
+n = int(input())
+d = {'Algorithm':204, 'DataAnalysis':207, 'ArtificialIntelligence':302, 'CyberSecurity':'B101', 'Network':303, 'Startup':501, 'TestStrategy':105}
+for i in range(n):
+    a = input()
+    print(d[a])

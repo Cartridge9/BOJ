590836ea590594a8555b89eac1a67ad3cd2c81c9
박재민 <96679652+Cartridge9@users.noreply.github.com>
2024-06-10 09:38:56 +0900

    [Bronze II] Title: 점수 집계, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ Python/백준/Bronze/9076. 점수 집계/점수 집계.py
@@ -0,0 +1,10 @@
+n = int(input())
+for i in range(n):
+    a = list(map(int, input().split()))
+    a.sort()
+    a.pop(0)
+    a.pop(-1)
+    if a[-1] - a[0] >= 4:
+        print("KIN") 
+    else:
+        print(sum(a))

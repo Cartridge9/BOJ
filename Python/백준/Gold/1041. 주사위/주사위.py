23a1a6eb9ae80418c05e18b0a5ed9c43df20f17a
박재민 <96679652+Cartridge9@users.noreply.github.com>
2024-12-10 12:36:46 +0900

    [Gold V] Title: 주사위, Time: 36 ms, Memory: 32544 KB -BaekjoonHub

--- Python/백준/Gold/1041. 주사위/주사위.py
+++ Python/백준/Gold/1041. 주사위/주사위.py
@@ -1,34 +1,30 @@
-import sys
-input = sys.stdin.readline
-
 n = int(input())
-a = list(map(int, input().split()))
-output = 0
-
+a, b, c, d, e, f = list(map(int, input().split()))
 if n == 1:
-    print(sum(a) - max(a))
-    exit()
+  print(a+b+c+d+e+f-max([a,b,c,d,e,f]))
+else:
+  paper = [[a, b, c], [a, d, e], [a, b, d], [a, e, c], [f, b, c], [f, d, e], [f, b, d], [f, e, c]]
+  # print(paper)
+  minpaper = [a, b, c]
+  for i in paper:
+    if sum(i) <= sum(minpaper):
+      minpaper = i
 
-s1 = (n - 2) ** 2 * 5 + 4 * (n - 2)
-s2 = (n - 2) * 8 + 4
-s3 = 4
-output += min(a) * s1
-k = 100
-for i in range(6):
-    for j in range(i + 1, 6):
-        if i + j == 5:
-            continue
-        k = min(k, a[i] + a[j])
-output += k * s2
-k = 150
-for i in range(6):
-    for j in range(i + 1, 6):
-        if i + j == 5:
-            continue
-        for l in range(j + 1, 6):
-            if i + l == 5 or j + l == 5:
-                continue
-            k = min(k, a[i] + a[j] + a[l])
-output += k * s3
+  # print(minpaper)
+  point = 8
+  border = n-2
+  inside = n**2-(point//2+border*4)
 
-print(output)
+  result = 0
+  minpaper.sort()
+  # print(minpaper)
+  result += sum(minpaper[0:3]) * point
+
+  result += sum(minpaper[0:2]) * border * 3 * 4
+
+  result += min(minpaper) * inside * 5
+
+  result -= minpaper[2] * 4
+  result -= minpaper[1] * (border * 4)
+
+  print(result)
